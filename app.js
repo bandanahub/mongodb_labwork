bfbcf9090cdd87b57a2e605b2c14a5edd09a44cf
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-09 15:01:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ const productSchema = new mongoose.Schema({
   });
   
   const Product = mongoose.model('Product', productSchema);
+
+// generateStarterData.js
+const mongoose = require('mongoose');
+const Product = require('./model'); // Importing the Product model
+
+mongoose.connect('mongodb://127.0.0.1:27017/mensWearDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  // Use the Product model here
+  const product = new Product({
+    name: 'Shirt',
+    price: 29.99,
+    category: 'Tops',
+    inStock: true
+  });
+
+  product.save().then(() => {
+    console.log('Product saved');
+    mongoose.connection.close();
+  });
+})
+.catch(err => console.error('Connection error', err));
